0560231c7c45845ad151e297b031428934f98f81
Ben Maher <ben@benjaminmaher.com>
2017-12-12 17:17:18 -0800

    add chartist, move to cloud9

--- flaskdash.py
+++ flaskdash.py
@@ -5,6 +5,7 @@ from flask_security import Security, SQLAlchemySessionUserDatastore, UserMixin, 
 from modules.database import *
 from modules.models import *
 import pandas as pd
+import os
 from flask_mail import Mail
 
 # Create app
@@ -45,16 +46,17 @@ def security_register_processor():
     return dict(company="company")
 
 #Create a user to test with
-@app.before_first_request
+#@app.before_first_request
 def create_user():
     init_db()
- #   mce = Client(name='Marin Clean Energy', domain='mcecleanenergy.org',abbreviation='MCE')
- #   svce = Client(name='Silicon Valley Clean Energy', domain='svcleanenergy.org',abbreviation='SVCE')
- #   db_session.add(svce)
-#    init_db()
- #   user_datastore.create_user(email='ben@benjaminmaher.com', password='password')
- #   user_datastore.create_user(email='bmaher@mahercpa.com', password='password')
- #   db_session.commit()
+    mce = Client(name='Marin Clean Energy', domain='mcecleanenergy.org',abbreviation='MCE')
+    svce = Client(name='Silicon Valley Clean Energy', domain='svcleanenergy.org',abbreviation='SVCE')
+    maher = Client(name='Maher Accountancy', domain='mahercpa.com',abbreviation='MAHER')
+    db_session.add(svce)
+    db_session.add(mce)
+    db_session.add(maher)
+    user_datastore.create_user(email='ben@benjaminmaher.com', password='password')
+    #db_session.commit()
 
 
 # Views
@@ -95,21 +97,42 @@ def dash():
     role_list = ','.join(role_list)
     return render_template('dash_content_test.html', usage_comparison=usage_comparison, dt_data = (dt), dt_cols = (col), role_names = role_list)
 
+@app.route("/data/chartist")
+def newchart():
+    return jsonify(datasources.chartist_data('pce'))
 
-@app.route("/morris")
-def morris():
-    return render_template('morris.html')
+@app.route("/newchart")
+def data_chartist():
+    return render_template('chartist.html')
+
+
+@app.route("/budget")
+def budget():
+    return render_template('budget_reports.html')
+
+@app.route("/pdf/budget")
+def budgetpdf():
+    fname = 'mce_2017-10_budget_reports.pdf'
+    return send_file(os.path.join(data_path, fname))
+
 
 from flask_security.signals import user_registered
 
 @user_registered.connect_via(app)
 def user_registered_sighandler(sender, user, confirm_token):
     print("print-user_registered_sighandler:", user.email)
-    user.company = 'test company'
-    db_session.commit()
+    user_domain = str(user.email).split('@')[1]
+    print(user_domain)
+    user_client = db_session.query(Client).filter_by(domain=user_domain).first()
+
+    if user_client is not None:
+        print(user_client.name)
+        user_client.users.append(user)
+        db_session.commit()
 
 
 user_registered.connect(user_registered_sighandler)
 
 if __name__ == '__main__':
     app.run()
+
